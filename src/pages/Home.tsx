1f278fadefce7ebcc367872e8a1f8ae0f042dbc3
chomba <chomba@bongohive.co.zm>
2022-10-15 22:54:59 +0200

    🦾 Initialise projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,6 +19,29 @@ export const HomePage = () => {
         </div>
       </nav>
 
+      <div className='hero-section'>
+        <div className='hero-text'>
+          <h4 className='hero-header'>
+            Start your journey
+            by one click, explore beautiful world!
+          </h4>
+          <p>
+            Plan and book your perfect trip with expert advice, travel tips, destination information and inspiration from us!
+          </p>
+          <ul className='download-icons'>
+            <li>
+              <img src='../../assets/images/playstore.png' alt='google-play' />
+            </li>
+            <li>
+              <img src='../assets/images/istore.png' alt='app-store' />
+            </li>
+          </ul>
+        </div>
+        <div className='hero-image'>
+          <img src="../assets/images/hero.png" alt="hero section" />
+        </div>
+      </div>
+
     </div>
   )
 }
